2a35f12d96a426654c27039b3cde7f34570fced2
issaysa <issaysa@gmail.com>
2012-08-07 07:59:41 +0900

    Update exercise2-1/template.js

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -12,11 +12,11 @@ Template.prototype = {
     	for ( keys in variables ) {
     		var str = variables[keys];
     		var escapedStr = str.replace(/<|>|&|'|"|\s/g, function(s){
-  			 	var map = {"<":"&lt;", ">":"&gt;", "&":"&amp;", "'":"&#39;", "\"":"&quot;", " ":"&nbsp;"};
-  			 	return map[s];
-  			});
-			variables[keys] = escapedStr;
-		}
+  			 var map = {"<":"&lt;", ">":"&gt;", "&":"&amp;", "'":"&#39;", "\"":"&quot;", " ":"&nbsp;"};
+  			 return map[s];
+  		});
+		variables[keys] = escapedStr;
+	}
     	var html = template.replace(/{%\s(\w+)\s%}/g, function(m, key) {
     		var text = variables[key] || '';
     		return text;
